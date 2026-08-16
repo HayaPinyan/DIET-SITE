68ec673114dada6256b1c9259d11a7cc2129cebc
HayaPinyan <h44640410@gmail.com>
2026-08-16 18:38:40 +0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://tpibknhyuxnwxuyqjgrk.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_GBaWTTt-FPDyUmqPJwDVOw_2PEZFMOo";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
